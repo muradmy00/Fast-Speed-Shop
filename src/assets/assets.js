1c59c293189dfae1a774aa38a5e4efc12511d191
muradmy00 <murad.my00@gmail.com>
2024-10-22 01:11:27 +0600

    code

---
+++ src/assets/assets.js
@@ -0,0 +1,206 @@
+import p_img1 from './p_img1.png'
+import p_img2 from './p_img2.png'
+import p_img3 from './p_img3.png'
+import p_img4 from './p_img4.png'
+import p_img5 from './p_img5.png'
+import p_img6 from './p_img6.png'
+import p_img7 from './p_img7.png'
+import p_img8 from './p_img8.png'
+import p_img9 from './p_img9.png'
+import p_img10 from './p_img10.png'
+
+
+
+
+import logo from './logo.png'
+import hero_img from './hero_img.png'
+import cart_icon from './cart_icon.png'
+import bin_icon from './bin_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import exchange_icon from './exchange_icon.png'
+import profile_icon from './profile_icon.png'
+import quality_icon from './quality_icon.png'
+import search_icon from './search_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+import star_icon from './star_icon.png'
+import support_img from './support_img.png'
+import menu_icon from './menu_icon.png'
+import about_img from './about_img.png'
+import contact_img from './contact_img.png'
+import razorpay_logo from './razorpay_logo.png'
+import stripe_logo from './stripe_logo.png'
+import cross_icon from './cross_icon.png'
+
+export const assets = {
+    logo,
+    hero_img,
+    cart_icon,
+    dropdown_icon,
+    exchange_icon,
+    profile_icon,
+    quality_icon,
+    search_icon,
+    star_dull_icon,
+    star_icon,
+    bin_icon,
+    support_img,
+    menu_icon,
+    about_img,
+    contact_img,
+    razorpay_logo,
+    stripe_logo,
+    cross_icon
+}
+
+export const products = [
+    {
+        _id: "aaaaa",
+        name: "Kitchen paper towel shelf",
+        description: "All item is 100% solid",
+        price: 2490,
+        offer: 1900,
+        Material:"Aluminum Alloy",
+        Size: "27.9*11cm",
+        image: [p_img1],
+        category: "Women",
+        subCategory: "Topwear",
+        color: ["Black", "White"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaab",
+        name: "New Flip Door Sink Rack",
+        description: "All item is 100% solid. Weight: 8.8kg. WaterProof Rustproof/Washable " ,
+        price: 14990,
+        offer: 11990,
+        Material: " Body carbon steel , acrylic door",
+        
+        image: [p_img2],
+        category: "Women",
+        subCategory: "Topwear",
+        color: ["Black", "White"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaac",
+        name: "Wall Mounted Dish Drain Rack",
+        description: "All item is 100% solid",
+        price: 7490,
+        offer: 6990,
+        Material: "304 Stainless Steel",
+        Size: "80*30*20cm",
+        image: [p_img3],
+        category: "Women",
+        subCategory: "Topwear",
+        color: ["steel as same picture Products"],
+        date: 1716234545448,
+        bestseller: true
+    },
+    {
+        _id: "aaaad",
+        name: "Luxurious Foldable Multi Hook Organizer",
+        description: "All item is 100% solid",
+        price: 2990,
+        offer: 2490,
+        Material: "Space Aluminum",
+        Size: "27.9*11cm",
+        image: [p_img4],
+        category: "Women",
+        subCategory: "Topwear",
+        color: ["Black", "Gold"],
+        date: 1716621345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaae",
+        name: "Unique Kitchen Cabinet",
+        description: "All item is 100% solid.weight:22.8kg",
+        price: 27990,
+        offer: 24990,
+        Material: "Carbon Steel",
+        Size: "L80/70",
+        image: [p_img5],
+        category: "Women",
+        subCategory:"Topwear",
+        color: ["Black", "Grey"],
+        date: 1716622345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaf",
+        name: "Kitche Cabinet Drawer Storage Cutlery Box",
+        description: "All item is 100% solid",
+        price: 1990,
+        offer: 2490,
+        Material: "Aluminum",
+        Size: "L41xW15x6cm",
+        image: [p_img6],
+        category:"Women",
+        subCategory: "Topwear",
+        color: ["Gun Grey"],
+        date: 1716623423448,
+        bestseller: true
+    },
+    {
+        _id: "aaaag",
+        name: "Modern Cabinet Countertop Spice Rack.",
+        description: "All item is 100% solid.weight:7.8kg",
+        price: 11990,
+        offer: 14990,
+        Material: "Aluminum",
+        Size: "L40.5xW28x70cm",
+        image: [p_img7],
+        category: "Women",
+        subCategory: "Topwear",
+        color: ["Black", "White"],
+        date: 1716621542448,
+        bestseller: false
+    },
+    {
+        _id: "aaaah",
+        name: "Modern Kitchen Cabinet.",
+        description: "All item is 100% solid.weight:19.00kg",
+        price: 21990,
+        offer: 24990,
+        Material: "carbon steel",
+        Size: "L-80 W-32 H-150cm",
+        image: [p_img8],
+        category: "Women",
+        subCategory: "Topwear",
+        color: ["black"],
+        date: 1716622345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaai",
+        name: "Kitchen Cabinet with drawer .",
+        description: "All item is 100% solid .weight:27.5kg",
+        price: 39990,
+        offer: 34990,
+        Material: "carbon steel",
+        Size: "L-80 W-40 H-135cm",
+        image: [p_img9],
+        category: "Women",
+        subCategory: "Topwear",
+        color: ["Black", "White"],
+        date: 1716621235448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaj",
+        name: "wall-mounted bathroom shelves",
+        description: "All item is 100% solid .",
+        price: 39990,
+        offer: 34990,
+        Material: "space aluminum",
+        Size: "L-80 W-40 H-135cm",
+        image: [p_img10],
+        category: "Women",
+        subCategory: "Topwear",
+        color: ["black"],
+        date: 1716622235448,
+        bestseller: false
+    },
+]
